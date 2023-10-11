364117a4fdc0a32f3a49ff3b7b755990fb53bb7e
Arjun Suthaharan <arjunsutha@gmail.com>
2023-10-11 18:57:35 -0400

    Initial Commit

---
+++ SwiftUI_APICalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_APICalls
+//
+//  Created by Arjun Suthaharan on 2023-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_APICalls/SwiftUI_APICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_APICallsApp.swift
+//  SwiftUI_APICalls
+//
+//  Created by Arjun Suthaharan on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_APICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
